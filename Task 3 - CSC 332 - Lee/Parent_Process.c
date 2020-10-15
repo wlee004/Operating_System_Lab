edc39dfd1ae0a856b6fabe25770dc6baa0db2a1f
wlee004 <wlee004@citymail.cuny.edu>
2020-10-14 23:26:49 -0400

    added sleep()

--- Task 3 - CSC 332 - Lee/Parent_Process.c
+++ Task 3 - CSC 332 - Lee/Parent_Process.c
@@ -6,7 +6,7 @@
 int main(){
 	
 	int child1, child2; 
-	char * arg[2];
+	
 	// fork first child 
 	child1 = fork();
 	if(child1 == -1){ 
@@ -14,12 +14,13 @@ int main(){
 		return 1;
 	}
 	if(child1 == 0){ // child 1 process
-	/*
+		char *arg[2];
 		arg[0] = "Process_P1";
 		arg[1] = NULL;
 		execv("Process_P1",arg);
-	*/
-		// fork second child 
+	}
+	else{
+		sleep(1);
 		child2 = fork();
 		if(child2 == -1){
 			printf("Failed to fork second child\n");
@@ -29,20 +30,13 @@ int main(){
 			char * arg2[2];
 			arg2[0] = "Process_P2";
 			arg2[1] = NULL;
-			execv("Process_P2", arg);
+			execv("Process_P2", arg2);
 		}
-		else{
-			char* arg[2];
-			arg[0] = "Process_P1";
-			arg[1] = NULL;
-			execv("Process_P1", arg);
-		}
-	}
-	else{ 
-		// parent process
+		else{ // parent process
 		int status1, status2; 
 		waitpid(child1, &status1, 0);
 		waitpid(child2, &status2, 0);
+		}
 	}
 	
 
