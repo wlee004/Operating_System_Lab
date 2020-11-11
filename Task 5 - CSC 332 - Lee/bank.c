d4260325ca14a282fd2f2df7e982f51b70149325
wlee004 <wlee004@citymail.cuny.edu>
2020-11-10 23:56:44 -0500

    finished  part 1 and 2

--- Task 5 - CSC 332 - Lee/bank.c
+++ Task 5 - CSC 332 - Lee/bank.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <time.h>
 #include "sem.h"
 
 #define CHILD      			0  			/* Return value of child proc from fork call */
 #define TRUE       			0  
 #define FALSE      			1
 
-FILE *fp1, *fp2, *fp3, *fp4;			/* File Pointers */
+FILE *fp1, *fp2, *fp3, *fp4, *fpdad, *fpson1, *fpson2;	/* File Pointers */
 
 int main()
 {
@@ -21,13 +20,13 @@ int main()
 	int bal1, bal2;					       // Balance read by processes
 	int flag, flag1;					   // End of loop variables
 	int transaction;  		               // semaphore id  
-	double dad_time, son1_time, son2_time; // wait time per process 
+	int dad_time, son1_time, son2_time;    // time counter          
 
+	// creating semaphore 
 	if((transaction = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT)) == -1){
 		printf("Failed to create semaphore");
 		exit(1);
 	}
-
 	sem_create(transaction, 1);
 	
 	//Initialize the file balance to be $100
@@ -41,7 +40,25 @@ int main()
 	N_Att = 20;
 	fprintf(fp4, "%d\n", N_Att);
 	fclose(fp4);
-	
+
+	//Initialize the time for dad 
+	fpdad = fopen("dad_time", "w");
+	dad_time = 0;
+	fprintf(fpdad, "%d\n", dad_time);
+	fclose(fpdad);
+
+	//Initialize the time for son 1
+	fpson1 = fopen("son1_time", "w");
+	son1_time = 0;
+	fprintf(fpson1, "%d\n", son1_time);
+	fclose(fpson1);
+
+	//Initialize the time for son 2  
+	fpson2 = fopen("son2_time", "w");
+	son2_time = 0;
+	fprintf(fpson2, "%d\n", son2_time);
+	fclose(fpson2);
+
 	//Create child processes that will do the updates
 	if ((pid = fork()) == -1) 
 	{
@@ -52,12 +69,35 @@ int main()
 	
 	if (pid == CHILD){
 	//First Child Process. Dear old dad tries to do some updates.
-	
+
 		N=5;
 		for(i=1;i<=N; i++)
 		{   
 			P(transaction); // start critical section 
-			printf("HERE");
+
+			fp3 = fopen("attempt" , "r+");
+			fscanf(fp3, "%d", &N_Att);
+						
+			// increment son 1 wait time 
+			if(N_Att > 1){
+				fpson1 = fopen("son1_time", "r+");
+				fscanf(fpson1,"%d", &son1_time);
+				fseek(fpson1,0L, 0);
+				son1_time += 1;
+				fprintf(fpson1, "%d\n", son1_time);
+				fclose(fpson1);
+			}
+
+			// increment son 2 wait time 
+			if(N_Att){
+				fpson2 = fopen("son2_time", "r+");
+				fscanf(fpson2,"%d", &son2_time);
+				fseek(fpson2,0L, 0);
+				son2_time += 1;
+				fprintf(fpson2, "%d\n", son2_time);
+				fclose(fpson2);
+			}
+			
 			printf("Dear old dad is trying to do update.\n");
 			fp1 = fopen("balance", "r+");
 			fscanf(fp1, "%d", &bal2);
@@ -75,8 +115,7 @@ int main()
 
 			V(transaction); // end critcal section 
 
-			sleep(rand()%5);	/* Go have coffee for 0-4 sec. */
-			
+			sleep(rand()%5);	/* Go have coffee for 0-4 sec. */	
 		}
 	}
 	
@@ -95,11 +134,31 @@ int main()
 			//Second child process. First poor son tries to do updates.
 			flag = FALSE;
 			while(flag == FALSE) 
-			{
+			{	
 				P(transaction); // start critical section 
 
 				fp3 = fopen("attempt" , "r+");
 				fscanf(fp3, "%d", &N_Att);
+				
+				// increment son 2 wait time 
+				if(N_Att > 1){
+					fpson2 = fopen("son2_time", "r+");
+					fscanf(fpson2,"%d", &son2_time);
+					fseek(fpson2,0L, 0);
+					son2_time += 1;
+					fprintf(fpson2, "%d\n", son2_time);
+					fclose(fpson2);
+				}
+
+				// increment dad wait time 
+				fpdad = fopen("dad_time", "r+");
+				fscanf(fpdad,"%d", &dad_time);
+				fseek(fpdad,0L, 0);
+				dad_time += 1;
+				fprintf(fpdad, "%d\n", dad_time);
+				fclose(fpdad);
+
+				
 				if(N_Att == 0)
 				{
 					fclose(fp3);
@@ -154,6 +213,27 @@ int main()
 
 					fp3 = fopen("attempt" , "r+");
 					fscanf(fp3, "%d", &N_Att);
+
+					// increment son 1 wait time 
+					if(N_Att > 1){
+						fpson1 = fopen("son1_time", "r+");
+						fscanf(fpson1,"%d", &son1_time);
+						fseek(fpson1,0L, 0);
+						son1_time += 1;
+						fprintf(fpson1, "%d\n", son1_time);
+						fclose(fpson1);
+					}
+					
+
+					// increment dad wait time 
+					fpdad = fopen("dad_time", "r+");
+					fscanf(fpdad,"%d", &dad_time);
+					fseek(fpdad,0L, 0);
+					dad_time += 1;
+					fprintf(fpdad, "%d\n", dad_time);
+					fclose(fpdad);
+
+					
 					if(N_Att == 0)
 					{
 						fclose(fp3);
@@ -194,15 +274,33 @@ int main()
 				//Now parent process waits for the child processes to finish
 				pid = wait(&status);
 				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
-			
+
+				fpdad = fopen("dad_time", "r+");
+				fscanf(fpdad,"%d", &dad_time);
+				fclose(fpdad);
+
 				pid = wait(&status);
 				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
+
+				fpson1 = fopen("son1_time", "r+");
+				fscanf(fpson1,"%d", &son1_time);
+				fclose(fpson1);
 			
 				pid = wait(&status);
 				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
 
+				fpson2 = fopen("son2_time", "r+");
+				fscanf(fpson2,"%d", &son2_time);
+				fclose(fpson2);
+
+				printf("Process Wait Time\n");
+				printf("N_Att: %d\n", N_Att);
+				printf("DAD: %d times\n", dad_time);
+				printf("SON 1: %d times\n", son1_time);
+				printf("SON 2: %d times\n", son2_time);
+
 				semkill(transaction);
-				semkill(attempt);
+
 			}
 			exit(0);
 		}
---
+++ Task 5 - CSC 332 - Lee/report.md
@@ -0,0 +1,36 @@
+# Task 5 - Process Synchronization 
+## William Lee 
+
+Screenshot of Part 1 
+
+![Part 1](part1.PNG)
+
+Here with the implmentation of synchronization through semaphores in part 1, you can see that each process is able to complete their update to the bank without getting overwritten by another process. 
+
+***
+
+Screenshot of Part 2, N = 10 
+
+![Part 2 Screenshot N = 10](n_10.PNG)
+
+Screenshot of Part 2, N = 12 
+
+![Part 2 Screenshot N = 12](n_12.PNG)
+
+Screenshot of Part 2, N = 14 
+
+![Part 2 Screenshot N = 14](n_14.PNG)
+
+Screenshot of Part 2, N = 16 
+
+![Part 2 Screenshot N = 16](n_16.PNG)
+
+Screenshot of Part 2, N = 18 
+
+![Part 2 Screenshot N = 18](n_18.PNG)
+
+Screenshot of Part 2, N = 20 
+
+![Part 2 Screenshot N = 20](n_20.PNG)
+
+Here N is the number of attempts the son processes can update the bank account. The screenshots show the output of the amount of times each process waited during the execution of the entire program. 
